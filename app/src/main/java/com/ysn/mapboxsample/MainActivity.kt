6b27ead848320dcd290c78a4edf4226c18e35cc4
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-02-24 16:56:48 +0700

    Example Program 1

--- app/src/main/java/com/ysn/mapboxsample/MainActivity.kt
+++ app/src/main/java/com/ysn/mapboxsample/MainActivity.kt
@@ -1,12 +1,108 @@
 package com.ysn.mapboxsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    lateinit var permissionsManager: PermissionsManager
+    lateinit var mapboxMap: MapboxMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.map_box_access_token))
         setContentView(R.layout.activity_main)
+        initMapView(savedInstanceState)
+        initPermissions()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map_view.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map_view.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map_view.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        map_view.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map_view.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        map_view.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        map_view.onSaveInstanceState(outState!!)
+    }
+
+    private fun initMapView(savedInstanceState: Bundle?) {
+        map_view.onCreate(savedInstanceState)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        this.mapboxMap.setStyle(Style.MAPBOX_STREETS)
+    }
+
+    private fun initPermissions() {
+        val permissionListener = object : PermissionsListener {
+            override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+                /* Nothing to do in here */
+            }
+
+            override fun onPermissionResult(granted: Boolean) {
+                if (granted) {
+                    syncMapbox()
+                } else {
+                    val alertDialogInfo = AlertDialog.Builder(this@MainActivity)
+                        .setTitle(getString(R.string.info))
+                        .setCancelable(false)
+                        .setMessage(getString(R.string.permissions_denied))
+                        .setPositiveButton(getString(R.string.dismiss)) { _, _ ->
+                            finish()
+                        }
+                        .create()
+                    alertDialogInfo.show()
+                }
+            }
+        }
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            syncMapbox()
+        } else {
+            permissionsManager = PermissionsManager(permissionListener)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun syncMapbox() {
+        map_view.getMapAsync(this)
     }
 }
